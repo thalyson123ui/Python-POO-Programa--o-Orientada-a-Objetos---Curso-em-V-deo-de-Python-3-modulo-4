1b1fc44a0bee518fd644771d2648b8341d0b75f0
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-01-19 12:10:34 -0300

    atualização

--- exercisios/exercisio002/ex002.py
+++ exercisios/exercisio002/ex002.py
@@ -1,28 +1,22 @@
 # declaração de classes
-class gafanhotos:
-    def __init__(self): # metodo construtor
-        self.nome = ""
-        self.idade = 0
+class Gafanhoto: 
+    def__init__(self, n = "vazio", i = 0):
+    # atributos de instancia
+    self.nome = n
+    self.idade = i
 
-    # metodos de instancia
+    # métodos de instancia
     def aniversario(self):
-        self.idade = self.idade + 1
+        self.idade = self.idade +1
 
-    def menssagem(self):
-        return f"{self.nome} é gafanhoto(a) e tem {self.idade} anos de idade."
+    def messagem(self):
+        return f"{self.name} é Gafanhoto(a) e tem {self.idade} anos de idade."
     
 # declaração de objetos
-g1 = gafanhotos()
-g1.nome = "João"
-g1.idade = 20
+g1 = Gafanhoto(n"maria", i= 22)
 g1.aniversario()
-print(g1.menssagem())
+print(g1.messagem())
 
-g2 = gafanhotos()
-g2.nome = "Maria"
-g2.idade = 25
+g2 = Gafanhoto(n="joão", i= 20)
 g2.aniversario()
-print(g2.menssagem())
-
-g3 = gafanhotos()
-print(g3.menssagem())
+print(g2.messagem())
