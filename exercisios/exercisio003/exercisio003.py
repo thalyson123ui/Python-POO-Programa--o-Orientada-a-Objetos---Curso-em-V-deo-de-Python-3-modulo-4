cba210d46f15b01c88e5dc680eea58acaeede500
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-01-19 14:25:48 -0300

    atualizar

--- exercisios/exercisio003/exercisio003.py
+++ exercisios/exercisio003/exercisio003.py
@@ -10,6 +10,23 @@ class ContaBancaria:
     def __str__(self):
         return f"A conta {self.id} de {self.titular} tem saldo de R${self.saldo:.2f} reais"
 
+    def depositar(self, valor):
+        if valor > 0:
+            self.saldo += valor
+        else:
+            print("Valor de depósito inválido")
 
+    def sacar(self, valor):
+        if valor <= 0:
+            print("Valor de saque inválido")
+        elif valor > self.saldo:
+            print("Saldo insuficiente")
+        else:
+            self.saldo -= valor
+
+
+# teste
 c1 = ContaBancaria(id=145, nome="Rita", saldo=7550)
+c1.depositar(500)
+c1.sacar(300)
 print(c1)
